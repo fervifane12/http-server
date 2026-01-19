40c441f7c7547a206787f21284534ea229d4af78
fervifane12 <fervifane12@gmail.com>
2026-01-19 15:33:31 -0500

    echo

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -6,15 +6,17 @@ public class Parser {
     public Parser() {
     }
 
-    public String parser(String string){
-        String[] splitString = string.split("/");
-        for (String stringPart:splitString){
-            if (!splitString[1].equals(" HTTP")){
-                return "HTTP/1.1 404 Not Found\r\n\r\n";
-            } else {
-                return "HTTP/1.1 200 OK\r\n\r\n";
-            }
+    public String parser(String string) {
+        String[] splitString = string.split(" ", 0);
+        System.out.println(Arrays.toString(splitString));
+
+        if (splitString[1].contains("/echo")) {
+            String[] string1 = splitString[1].split("/", 0);
+            return "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + string1[2].length() + "\r\n\r\n" + string1[2];
+        } else if (splitString[1].equals("/")) {
+            return "HTTP/1.1 200 OK\r\n\r\n";
+        } else {
+            return "HTTP/1.1 404 Not Found\r\n\r\n";
         }
-        return null;
     }
 }
