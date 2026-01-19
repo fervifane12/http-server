61896f07a0daeb0b93574a67fb89a15bd066fb82
fervifane12 <fervifane12@gmail.com>
2026-01-19 11:26:34 -0500

    Creating the 200 OK response

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,12 +9,19 @@ public class Main {
      try {
        ServerSocket serverSocket = new ServerSocket(4221);
 
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
-       // ensures that we don't run into 'Address already in use' errors
+// Since the tester restarts your program quite often, setting SO_REUSEADDR
+// ensures that we don't run into 'Address already in use' errors
        serverSocket.setReuseAddress(true);
+       Socket socket = serverSocket.accept();
+       System.out.println("Accepted new connection");
 
-       serverSocket.accept(); // Wait for connection from client.
-       System.out.println("accepted new connection");
+       String response = "HTTP/1.1 200 OK\r\n\r\n";
+       OutputStream out = socket.getOutputStream();
+       out.write(response.getBytes());
+       out.flush();
+
+       socket.close();
+
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
