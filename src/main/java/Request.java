8e4c50b40db9c5e69e567b612c20281b82296618
fervifane12 <fervifane12@gmail.com>
2026-01-29 01:20:59 -0500

    Support for the content-encoding header

--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -6,6 +6,7 @@ public record Request(String httpMethod,
                       Map<String, String> headers,
                       String body) {
     public String getHeader(String key){
-        return !headers.get(key).isBlank() ? headers.get(key): null;
+        assert headers.get(key) != null;
+        return headers.get(key);
     }
 }
